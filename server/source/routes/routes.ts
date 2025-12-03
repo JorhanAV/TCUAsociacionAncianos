8b2bf598ae41c2bc8daec2d159060a1bd58fa49d
Jorhan <jorhanalfavar@gmail.com>
2025-12-03 15:22:34 -0600

    adding user module API

--- server/source/routes/routes.ts
+++ server/source/routes/routes.ts
@@ -4,24 +4,26 @@ import { HistorialInventarioRoutes } from "./historialInventario.routes";
 import { PerfilRoutes } from "./perfiles.routes";
 import { ActividadesRoutes } from "./actividades.routes";
 import { ImageRoutes } from "./images.routes";
-
+import { UsuarioRoutes } from "./usuario.routes";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     // ----Agregar las rutas----
-    // Ruta de inventario
+
+    // localhost:3000/usuario/
+    router.use("/usuario", UsuarioRoutes.routes);
 
     // localhost:3000/inventario/
     router.use("/inventario", InventarioRoutes.routes);
 
     router.use("/historial", HistorialInventarioRoutes.routes);
-     // localhost:3000/perfiles/
+    // localhost:3000/perfiles/
     router.use("/perfiles", PerfilRoutes.routes);
 
     router.use("/actividades", ActividadesRoutes.routes);
-     // localhost:3000/imagenes/
+    // localhost:3000/imagenes/
     router.use("/imagenes", ImageRoutes.routes);
     return router;
   }
