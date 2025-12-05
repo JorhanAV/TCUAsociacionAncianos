46ef523664d64afad8e061c0035b1cfd4ba07c16
Jorhan <jorhanalfavar@gmail.com>
2025-12-05 14:43:43 -0600

    adding files to user module

---
+++ app/src/app/user/screens/user-pass/user-pass.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-pass',
+  standalone: false,
+  templateUrl: './user-pass.html',
+  styleUrl: './user-pass.css',
+})
+export class UserPass {
+
+}
--- app/src/app/user/user-module.ts
+++ app/src/app/user/user-module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing-module';
+import { UserLogin } from './screens/user-login/user-login';
+import { UserForm } from './screens/user-form/user-form';
+import { UserPass } from './screens/user-pass/user-pass';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserLogin,
+    UserForm,
+    UserPass
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
