bd5e1019d7010866bfa97d5e9ec2e16809095e54
marioVMGIT <marioamurillo27@gmail.com>
2025-12-05 21:31:01 -0600

    working2

--- app/src/app/app-routing-module.ts
+++ app/src/app/app-routing-module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShellComponent } from './core/layout/shell.component';
 import { PageNotFound } from './share/page-not-found/page-not-found';
 import { UserLogin } from './user/screens/user-login/user-login';
+import { authGuard } from './share/auth.guard';
 
 const routes: Routes = [
   
   { path: 'user-login', component: UserLogin },
   {
     path: '',
+    canActivate: [authGuard],
     component: ShellComponent,
     children: [
       { path: '', redirectTo: 'inicio', pathMatch: 'full' },
