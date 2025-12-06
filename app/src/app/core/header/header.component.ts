0cb84f97d533835ba8c3bec6ff9fcc8184f66001
Jorhan <jorhanalfavar@gmail.com>
2025-12-05 19:30:34 -0600

    update

--- app/src/app/core/header/header.component.ts
+++ app/src/app/core/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
 import { AuthenticationService } from '../../share/authentication.service';
 import { MatDialog } from '@angular/material/dialog';
 import { UserPass } from '../../user/screens/user-pass/user-pass';
+import { NotificationsService } from '../../share/services/notification.service';
 
 type NotiType = 'success' | 'warning' | 'info' | 'error';
 interface Notificacion {
@@ -12,8 +13,6 @@ interface Notificacion {
   leido: boolean;
 }
 
-import { Component, EventEmitter, inject, Output } from '@angular/core';
-import { NotificationsService } from '../../share/services/notification.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -28,29 +27,21 @@ export class HeaderComponent {
   currentUser = this.auth.currentUserSignal;
 
   // signal reactiva (en lugar de array mutable)
-  notis = signal<Notificacion[]>([
-    { id: 1, texto: 'Jose Soto renovó el plan Ultimate', tipo: 'success', leido: false },
-    { id: 2, texto: 'Jose Soto depositó CRC 35000', tipo: 'success', leido: false },
-    { id: 3, texto: 'tony soto aceptó invitación', tipo: 'info', leido: false },
-    { id: 4, texto: 'tony soto es ahora un nuevo cliente', tipo: 'error', leido: true },
-  ]);
+ 
 
-  unreadCount = signal(0);
+  notis = this.notif.ultimas5;
+  unreadCount = this.notif.unreadCount;
 
   constructor(private dialog: MatDialog) {
-    this.updateUnread();
   }
-
+/* 
   private updateUnread() {
     const count = this.notis().filter((n) => !n.leido).length;
     this.unreadCount.set(count);
-  }
+  } */
 
-  marcarTodasLeidas(event?: Event) {
-    if (event) event.stopPropagation(); // Evita que el menú se cierre al hacer click
-
-    this.notis.update((arr) => arr.map((n) => ({ ...n, leido: true })));
-    this.updateUnread();
+  marcarTodasLeidas() {
+    this.notif.marcarTodas();
   }
   logout() {
     this.auth.logout();
@@ -60,12 +51,5 @@ export class HeaderComponent {
       width: '470px',
       panelClass: 'blur-modal',
     });
-  }
-}
-  notis = this.notif.ultimas5;
-  unreadCount = this.notif.unreadCount;
-
-  marcarTodasLeidas() {
-    this.notif.marcarTodas();
   }
 }
